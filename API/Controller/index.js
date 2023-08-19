3049812fd5a71e76ff079fd712030967d103e52d
RayhaanK <rayhaankhan331@gmail.com>
2023-08-19 10:37:57 +0200

    updates users

--- API/Controller/index.js
+++ API/Controller/index.js
@@ -1,1 +1,31 @@
 // Controller
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const {verrifyAToken} = require('../Middleware/AuthenticateUser')
+const routes = express.Router()
+const {users, books, bookAuthor} = require('../model')
+
+// ================User's Router=================
+routes.get('/users', (req, res)=>{
+    users.fetchUsers(req, res)
+})
+routes.get('/user/:id', (req, res)=>{
+    users.fetchUser(req, res)
+})
+routes.post('/register', bodyParser.json(), (req, res)=>{
+    users.register(req, res)
+})
+routes.post('/login',
+bodyParser.json(), (req, res)=>{
+    users.login(req, res)
+})
+routes.put('/user/:id', bodyParser.json(), (req, res)=>{
+    users.updateUser(req, res)
+})
+routes.patch('/user/:id', bodyParser.json(), (req, res)=>{
+    users.updateUser(req, res)
+})
+routes.delete('/user/:id', (req, res)=>{
+    users.deleteUser(req, res)
+})
--- API/model/index.js
+++ API/model/index.js
@@ -1,1 +1,6 @@
 // Index
+const Users = require('./Users')
+// Export all objects
+module.exports = {
+    users: new Users()
+}
